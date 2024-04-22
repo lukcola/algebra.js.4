87f72c575b29a5d106d39a3fd971aa454475ecaa
LP <lukcola10@gmail.com>
2024-04-22 18:56:24 +0200

    zadnji push

--- javascript.js
+++ javascript.js
@@ -44,3 +44,20 @@ for(var i=1; i<21; i++){
 }
 */
 
+var TableBegin = "<table><tbody>";
+var TableEnd = "</tbody></table>";
+
+var rowBegin = "<tr>";
+var rowEnd = "</tr>";
+
+var rowData = ''; //"<td>ime</td><td>prezime</td>"
+var row = "";
+
+for (var i = 0; i < 20; i++) {
+    row += rowBegin;
+    rowData = '<td>ime' + i + '</td><td>prezime' + i + '</td';
+    row += rowData;
+    row += rowEnd;
+}
+
+document.write(TableBegin + row + TableEnd);
