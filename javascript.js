17329ecd2b61de86a902d70dd3608a0a35946633
LP <lukcola10@gmail.com>
2024-04-22 18:05:57 +0200

    drugi push

---
+++ javascript.js
@@ -0,0 +1,18 @@
+/*
+var text_1 = `moje ime`
+var text_2 = `moje_ime`
+
+if (text_1 === text_2){
+    console.log("Varijable su identične!");
+} else {
+    console.log("Varijable nisu identične!");
+}
+
+var a = ["ime", "prezime"];
+console.log(a, typeof(a));
+
+var b = ["ime", 1, 2];
+console.log(b, typeof(b));
+*/
+
+
