75bde11ec518c1aeb44be8e3f34d61ea282615ad
LP <lukcola10@gmail.com>
2024-04-22 18:29:09 +0200

    treci push

--- javascript.js
+++ javascript.js
@@ -13,6 +13,34 @@ console.log(a, typeof(a));
 
 var b = ["ime", 1, 2];
 console.log(b, typeof(b));
-*/
 
+
+var firstName = "Ivan ";
+var lastName = " Horvat";
+
+var fullName = firstName.trim() + " " + lastName.trim();
+
+console.log(fullName);
+
+
+
+// var x = z == 2 ? y : 5;
+var z = 3;
+var y = 0;
+
+if (z === 2) {
+    x = y;
+} else {
+    x = 5;
+}
+
+console.log("x=" + x);
+
+
+for(var i=1; i<21; i++){
+    if(i % 2 == 0){
+        console.log(`Broj ${i} je paran!`);
+    }
+}
+*/
 
